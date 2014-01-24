b6a7b547bc76fcdf75946f5685f0afd9eeef4d7c
arata <arata@element-system.co.jp>
2014-01-24 19:10:44 +0900

    bump version

--- elm-post-headliner.php
+++ elm-post-headliner.php
@@ -4,7 +4,7 @@ Plugin Name: ELM Post Headliner
 Plugin URI: http://www.element-system.co.jp
 Description: 記事のヘッドライン表示用ショートコードを提供します。オプションの一覧はソースを参照。Usage: [headliner], [headliner category_name=hoge], [headliner post_type=piyo posts_per_page=n]
 Author: Yuki Arata
-Version: 1.2.0
+Version: 1.2.1
 Author URI: http://www.element-system.co.jp
 License: GPLv2 or later
 */
@@ -28,7 +28,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 class ElmPostHeadliner
 {
-	private $ver = '1.2.0';
+	private $ver = '1.2.1';
 
 	protected $defaults = array(
 		// Query Parameters
