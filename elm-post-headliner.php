b1ec1884a5fc1858b28efcf541ff4c73be251896
arata <arata@element-system.co.jp>
2015-06-18 13:53:53 +0900

    bump up version

--- elm-post-headliner.php
+++ elm-post-headliner.php
@@ -4,7 +4,7 @@ Plugin Name: ELM Post Headliner
 Plugin URI: https://bitbucket.org/elmadmin/elm-post-headliner
 Description: 記事のヘッドライン表示用ショートコードを提供します。Usage: [headliner] <a href="https://bitbucket.org/elmadmin/elm-post-headliner">&raquo;詳しい説明</a>
 Author: Yuki Arata
-Version: 1.4.5
+Version: 1.4.6
 Author URI: http://www.element-system.co.jp
 License: GPLv2 or later
 */
@@ -28,7 +28,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 class ElmPostHeadliner
 {
-	private $ver = '1.4.5';
+	private $ver = '1.4.6';
 
 	protected $defaults = array(
 		// Query Parameters
